a3369e862dd8dbb5fbbe7032ec5294a090833e29
zhouweikai <zhouweikaier@gmail.com>
2018-07-31 21:28:07 -0700

    “init”

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './Header';
+import { Main } from './Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <Main/>
       </div>
     );
   }
