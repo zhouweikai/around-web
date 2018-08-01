aa1cdda515fe8b7ce8ecf37c54506e08519ec398
zhouweikai <zhouweikaier@gmail.com>
2018-07-31 22:54:09 -0700

    add a line

--- src/Register.js
+++ src/Register.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, message } from 'antd';
+import $ from 'jquery';
+import { API_ROOT } from 'constants';
 
 const FormItem = Form.Item;
 
@@ -14,6 +16,20 @@ class RegistrationForm extends React.Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                   url: `${API_ROOT}/signup`,
+                   method: 'POST',
+                   data: JSON.stringify({
+                       username: values.username,
+                       password: values.password,
+                   }),
+                }).then((response) => {
+                    message.success(response);
+                }, (response) => {
+                    message.error(response.responseText);
+                }).catch((e) => {
+                    console.log(e);
+                });
             }
         });
     }
@@ -43,7 +59,7 @@ class RegistrationForm extends React.Component {
 
     render() {
         const { getFieldDecorator } = this.props.form;
-        const { autoCompleteResult } = this.state;
+        //const { autoCompleteResult } = this.state;
 
         const formItemLayout = {
             labelCol: {
